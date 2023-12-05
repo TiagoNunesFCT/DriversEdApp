e491efa9a0eac0b5dc376b09a444f2174a3f9d09
TiagoNunesFCT <tjf.nunes@campus.fct.unl.pt>
2023-12-05 17:32:58 +0000

    category file filled

---
+++ drivers_ed_app/lib/Model/category.dart
@@ -0,0 +1,30 @@
+
+//This class represents a Category object
+class Category {
+  //The Current Category Attributes
+  int categoryId;
+  String categoryName;
+  String categoryDescription;
+
+  Category({required this.categoryId, required this.categoryName, required this.categoryDescription});
+
+  //to be used when inserting a row in the table
+  Map<String, dynamic> toMapWithoutId() {
+    final map = <String, dynamic>{};
+    map["category_name"] = categoryName;
+    map["category_category"] = categoryDescription;
+    return map;
+  }
+
+  //to be used when updating a row in the table
+  Map<String, dynamic> toMap() {
+    final map = <String, dynamic>{};
+    map["category_id"] = categoryId;
+    map["category_name"] = categoryName;
+    map["category_category"] = categoryDescription;
+    return map;
+  }
+
+  //to be used when converting the row into object
+  factory Category.fromMap(Map<String, dynamic> data) => Category(categoryId: data['category_id'], categoryName: data['category_name'], categoryDescription: data['category_description']);
+}
